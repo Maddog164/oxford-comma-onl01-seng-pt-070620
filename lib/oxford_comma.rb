264c58a8f208dbb598b08278dd73fe871184f09c
Denise Henderson <denise@henderson3.us>
2020-07-09 13:34:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     newstring = array.join(" and ")
   else array.length > 2
     holdelement = array.pop(1) #holds last element
-    holdelement = "#{holdelement[0]}"
+    holdelement = "#{holdelement[0]}" #convert to string vs array
     newstring = array.join(", ")
     newstring = "#{newstring}" + ", and " + "#{holdelement}" #addback last element
   end
