f811911acc2100c7386344c665ccca5195f4186c
Denise Henderson <denise@henderson3.us>
2020-07-09 13:13:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def oxford_comma(array)
   if array.length==1
     newstring = "#{array[0]}"
@@ -5,6 +7,7 @@ def oxford_comma(array)
     newstring = array.join(" and ")
   elsif array.length ==3
     holdstring = array.pop(1) #holds array without last element
+    binding.pry
     newstring = holdstring.join(", ")
     newstring = "#{newstring}" + " and " + "#{array[2]}" #addback last element
   else
