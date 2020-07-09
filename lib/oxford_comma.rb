4607afb78fcdb4c145abb09520ec32d8a3d022dc
Denise Henderson <denise@henderson3.us>
2020-07-09 13:17:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
-require 'pry'
+#require 'pry'
 
 def oxford_comma(array)
   if array.length==1
@@ -6,10 +6,10 @@ def oxford_comma(array)
   elsif array.length==2
     newstring = array.join(" and ")
   elsif array.length ==3
-    holdstring = array.pop(1) #holds array without last element
-    binding.pry
-    newstring = holdstring.join(", ")
-    newstring = "#{newstring}" + " and " + "#{array[2]}" #addback last element
+    holdelement = array.pop(1) #holds last element
+    #binding.pry
+    newstring = array.join(", ")
+    newstring = "#{newstring}" + " and " + "#{holdelement}" #addback last element
   else
     newstring = array.join(", ")
   end
