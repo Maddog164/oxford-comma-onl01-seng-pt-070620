0837ade1485e6b376310b0b2ab0a5f5b58af7078
Denise Henderson <denise@henderson3.us>
2020-07-09 13:12:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length==1
+    newstring = "#{array[0]}"
+  elsif array.length==2
+    newstring = array.join(" and ")
+  elsif array.length ==3
+    holdstring = array.pop(1) #holds array without last element
+    newstring = holdstring.join(", ")
+    newstring = "#{newstring}" + " and " + "#{array[2]}" #addback last element
+  else
+    newstring = array.join(", ")
+  end
+  return newstring
 end
